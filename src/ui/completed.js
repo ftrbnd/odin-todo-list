95dc5c1f137308b4c237f615e6c10b34e1c01c19
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-03-21 20:52:34 -0700

    Fix completedIds parsing

--- src/ui/completed.js
+++ src/ui/completed.js
@@ -6,19 +6,20 @@ export default function displayCompleted() {
     completedDiv.appendChild(h2);
 
     let completedItemIds = localStorage.getItem('completed-items');
+    if (completedItemIds.length > 0)
+        completedItemIds = completedItemIds.split(',');
 
     const countDescription = document.createElement('h3');
     countDescription.textContent = updateCompletedCount(completedItemIds);
     countDescription.style.paddingLeft = '8px';
 
     countDescription.addEventListener('click', () => {
-        // show completed items
         console.log('Showing completed items...', completedItemIds);
         for (const itemId of completedItemIds) {
             if (!itemId) continue;
-
-            // do stuff
             console.log(`Found a completed item: ${itemId}`);
+            
+            // do stuff
         }
     });
 
@@ -26,15 +27,12 @@ export default function displayCompleted() {
 }
 
 function updateCompletedCount(completedItemIds) {
-    let description;
-    let completedCount = 0;
-    if (!completedItemIds) {
+    let description, completedCount = 0;
+    if (completedItemIds.length == 1) {
         description = '0 items';
     } else {
-        completedItemIds = completedItemIds.split(',');
         for (const itemId of completedItemIds) {
             if (!itemId) continue;
-
             completedCount += 1;
         }
         description = `${completedCount} items`;
